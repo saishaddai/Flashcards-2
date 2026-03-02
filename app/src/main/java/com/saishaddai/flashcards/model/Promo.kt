ee11753444e969a1c3f23c647ca4501980c38406
Saidel Lopez <sailopher@yahoo.com.mx>
2026-03-01 23:52:01 -0600

    added an interface for repositories so in the future I can switch with more interesting sources to get the decks and flashcards

---
+++ app/src/main/java/com/saishaddai/flashcards/model/Promo.kt
@@ -0,0 +1,11 @@
+package com.saishaddai.flashcards.model
+
+data class Promo (val title: String, val description: String, val deckId: Int)
+
+
+
+val promos  = listOf<Promo>(
+    Promo("Ready for a challenge?", "Learn about OOP", 1),
+    Promo("Ready for a start?", "Learn Android Core", 2),
+    Promo("Ready for a challenge?", "Learn Kotlin", 3),
+)

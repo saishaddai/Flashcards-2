a219f98288c41f9e5fff72a93637f8196d726011
Saidel Lopez <sailopher@yahoo.com.mx>
2026-03-02 23:27:54 -0600

    getting the right flashcards count from the flashcards source of truth. TODO add the mastery from the session summaries

--- app/src/main/java/com/saishaddai/flashcards/model/Deck.kt
+++ app/src/main/java/com/saishaddai/flashcards/model/Deck.kt
@@ -3,27 +3,31 @@ package com.saishaddai.flashcards.model
 data class Deck(
     val id : Int,
     val name: String,
-    val cardCount: Int,
     val mastery: Int,
+    var cardCount: Int = 0,
     val isSelected: Boolean = false
 )
 
 val decks = listOf(
-    Deck(1, "OOP", 124, 80),
-    Deck(2, "Android Core", 210, 45, true),
-    Deck(3, "Kotlin", 90, 45),
-    Deck(4, "Kotlin MP", 88, 12),
-    Deck(5, "Security", 56, 0),
-    Deck(6, "Compose", 150, 62),
-    Deck(7, "Databases", 94, 33),
-    Deck(8, "Dagger/Hilt", 20, 0),
-    Deck(9, "Material 3", 4, 0),
-    Deck(10, "Navigation", 4, 0),
-    Deck(11, "Jetpack", 4, 0),
-    Deck(12, "Unit Test", 4, 0),
-    Deck(13, "Gradle", 4, 0),
-    Deck(14, "Android OPS", 4, 0),
-    Deck(15, "Libraries", 4, 0),
-    Deck(16, "Design Patterns", 4, 0),
-    Deck(17, "Coroutines", 4, 0)
-)
+    Deck(1, "OOP", 80),
+    Deck(2, "Android Core", 45, isSelected = true),
+    Deck(3, "Kotlin", 45),
+    Deck(4, "Kotlin MP", 12),
+    Deck(5, "Security", 0),
+    Deck(6, "Compose", 62),
+    Deck(7, "Databases", 33),
+    Deck(8, "Dagger/Hilt", 0),
+    Deck(9, "Material 3", 0),
+    Deck(10, "Navigation", 0),
+    Deck(11, "Jetpack", 0),
+    Deck(12, "Unit Test", 0),
+    Deck(13, "Gradle", 0),
+    Deck(14, "Android OPS", 0),
+    Deck(15, "Libraries", 0),
+    Deck(16, "Design Patterns", 0),
+    Deck(17, "Coroutines", 0)
+).map {
+    it.apply {
+        cardCount = flashcards.count { flashcard -> flashcard.deckId == it.id }
+    }
+}

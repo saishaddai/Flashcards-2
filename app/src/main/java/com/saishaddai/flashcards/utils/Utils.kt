8dd003f94e12a0805e4f15990861be6328412bd2
Saidel Lopez <sailopher@yahoo.com.mx>
2026-03-03 13:09:30 -0600

    improved the mastery names to include more levels and leaving Mastered only when getting the 100%

--- app/src/main/java/com/saishaddai/flashcards/utils/Utils.kt
+++ app/src/main/java/com/saishaddai/flashcards/utils/Utils.kt
@@ -11,8 +11,10 @@ fun List<Flashcard>.random(size: Int): List<Flashcard> = shuffled().take(size)
 fun Deck.getMasteryLevel(): Int {
     return when (mastery) {
         in -1 .. 0 -> R.string.mastery_level_not_started
-        in 1..25 -> R.string.mastery_level_just_started
-        in 26..75 -> R.string.mastery_level_getting_there
+        in 1..25 -> R.string.mastery_level_novice
+        in 26..50 -> R.string.mastery_level_sophomore
+        in 51..75 -> R.string.mastery_level_experienced
+        in 76..99 -> R.string.mastery_level_veteran
         else -> R.string.mastery_level_mastered
     }
 }

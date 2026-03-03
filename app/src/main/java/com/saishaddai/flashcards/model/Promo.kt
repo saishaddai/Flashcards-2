f01c75727e78e5119f8bd9a38cf3d09bb2f6a1c1
Saidel Lopez <sailopher@yahoo.com.mx>
2026-03-02 23:19:03 -0600

    now DecksViewModel is connected to Decks Screen. TODO get the real information from session summaries and the number of flashcards per deck

--- app/src/main/java/com/saishaddai/flashcards/model/Promo.kt
+++ app/src/main/java/com/saishaddai/flashcards/model/Promo.kt
@@ -2,9 +2,7 @@ package com.saishaddai.flashcards.model
 
 data class Promo (val title: String, val description: String, val deckId: Int)
 
-
-
-val promos  = listOf<Promo>(
+val promos  = listOf(
     Promo("Ready for a challenge?", "Learn about OOP", 1),
     Promo("Ready for a start?", "Learn Android Core", 2),
     Promo("Ready for a challenge?", "Learn Kotlin", 3),

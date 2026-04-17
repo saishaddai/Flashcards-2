a6a9f5fd0a870ce2bfe57f3ad7b3c742e13a7c26
Saidel Lopez <sailopher@yahoo.com.mx>
2026-04-17 17:15:55 -0600

    updated all the pending versions to update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,9 +6,7 @@ plugins {
 
 android {
     namespace = "com.saishaddai.flashcards"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.saishaddai.flashcards"

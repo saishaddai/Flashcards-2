4074c40d410c4ffe9b5493f14de281867261b4c8
Saidel Lopez <sailopher@yahoo.com.mx>
2026-07-20 12:49:54 -0600

    added more instrumentation tests to FicnishSessionScreen

--- app/src/androidTest/java/com/saishaddai/flashcards/screens/FinishSessionScreenTest.kt
+++ app/src/androidTest/java/com/saishaddai/flashcards/screens/FinishSessionScreenTest.kt
@@ -3,11 +3,17 @@ package com.saishaddai.flashcards.screens
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.v2.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performScrollTo
 import com.saishaddai.flashcards.model.Deck
+import com.saishaddai.flashcards.model.MasteryLevel
 import com.saishaddai.flashcards.ui.theme.Flashcards2Theme
+import com.saishaddai.flashcards.utils.SessionResult
+import com.saishaddai.flashcards.utils.TestTags
+import com.saishaddai.flashcards.utils.UiState
+import com.saishaddai.flashcards.viewmodel.FinishSessionUiData
 import org.junit.Rule
 import org.junit.Test
 
@@ -21,6 +27,122 @@ class FinishSessionScreenTest {
         name = "OOP",
         longName = "Object Oriented Programming"
     )
+
+    @Test
+    fun finishSessionScreen_loadingState_showsLoader() {
+        composeTestRule.setContent {
+            Flashcards2Theme {
+                FinishSessionScreen(
+                    uiState = UiState.Loading,
+                    deck = testDeck,
+                    cardsReviewed = 0,
+                    startTime = 0L,
+                    endTime = 0L,
+                    totalTimeMillis = 0L,
+                    onFinishSession = {},
+                    onShareSummary = {},
+                    onBackToDecksClicked = {},
+                    onNavigationHandled = {},
+                    onRetry = {}
+                )
+            }
+        }
+
+        composeTestRule.onNodeWithTag(TestTags.FULL_LOADER).assertIsDisplayed()
+    }
+
+    @Test
+    fun finishSessionScreen_errorState_showsErrorViewAndRetries() {
+        var retryCalled = false
+        composeTestRule.setContent {
+            Flashcards2Theme {
+                FinishSessionScreen(
+                    uiState = UiState.Error("Session Save Failed"),
+                    deck = testDeck,
+                    cardsReviewed = 0,
+                    startTime = 0L,
+                    endTime = 0L,
+                    totalTimeMillis = 0L,
+                    onFinishSession = {},
+                    onShareSummary = {},
+                    onBackToDecksClicked = {},
+                    onNavigationHandled = {},
+                    onRetry = { retryCalled = true }
+                )
+            }
+        }
+
+        composeTestRule.onNodeWithText("Oops!").assertIsDisplayed()
+        composeTestRule.onNodeWithText("Session Save Failed").assertIsDisplayed()
+        composeTestRule.onNodeWithText("Retry").performClick()
+
+        assert(retryCalled)
+    }
+
+    @Test
+    fun finishSessionScreen_successState_handlesNavigation() {
+        var finishSessionCalled = false
+        var navigationHandledCalled = false
+        composeTestRule.setContent {
+            Flashcards2Theme {
+                FinishSessionScreen(
+                    uiState = UiState.Success(
+                        FinishSessionUiData(
+                            sessionResult = null,
+                            navigateToDeckList = true
+                        )
+                    ),
+                    deck = testDeck,
+                    cardsReviewed = 10,
+                    startTime = 0L,
+                    endTime = 1000L,
+                    totalTimeMillis = 1000L,
+                    onFinishSession = { finishSessionCalled = true },
+                    onShareSummary = {},
+                    onBackToDecksClicked = {},
+                    onNavigationHandled = { navigationHandledCalled = true },
+                    onRetry = {}
+                )
+            }
+        }
+
+        assert(finishSessionCalled)
+        assert(navigationHandledCalled)
+    }
+
+    @Test
+    fun finishSessionContent_withResult_showsAchievements() {
+        val result = SessionResult(
+            sessionProgress = 15.0,
+            newProgress = 65.0,
+            masteryLevel = MasteryLevel.EXPERIENCED
+        )
+
+        composeTestRule.setContent {
+            Flashcards2Theme {
+                FinishSessionContent(
+                    deck = testDeck,
+                    cardsReviewed = 20,
+                    startTime = 0L,
+                    endTime = 1000L,
+                    totalTimeMillis = 1000L,
+                    sessionResult = result,
+                    onBackToDecksClicked = {},
+                    onShareSummary = {}
+                )
+            }
+        }
+
+        // Verify XP achievement
+        composeTestRule.onNodeWithText("Total Experience: +15 XP", substring = true).assertIsDisplayed()
+
+        // Verify Level achievement
+        composeTestRule.onNodeWithText("Your current level: Experienced", substring = true).assertIsDisplayed()
+
+        // Verify other achievements (currently hardcoded in UI but should be visible)
+        composeTestRule.onNodeWithText("Weekly Time: 25 minute(s)", substring = true).assertIsDisplayed()
+        composeTestRule.onNodeWithText("Daily Goal Streak: 3 day(s)", substring = true).assertIsDisplayed()
+    }
 
     @Test
     fun finishSessionContent_initialState_showsSummaryInfo() {
